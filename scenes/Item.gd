77de74d0d3be2686a1e1b6619a1a50fc732faa2e
Clouds Flowing <clouds.flowing@gmail.com>
2023-03-03 18:40:55 +0800

    add click

--- scenes/Item.gd
+++ scenes/Item.gd
@@ -4,14 +4,20 @@ var x = 0
 var y = 0
 var map = null
 
+func _on_control_gui_input(event):
+  if event is InputEventMouseButton:
+    if event.is_pressed():
+      self.map.uncover(self.x)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
   pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-  if self.map.generated:
+  if not self.map.generated:
+    return
+  if self.map.cover[self.x] == 1:
     $fg.show()
     var d = self.map.map[self.x]
     if d == -1:
@@ -19,3 +25,7 @@ func _process(_delta):
     elif d != 0:
       $data.text = str(d)
       $data.show()
+    else:
+      $data.text = ""
+      pass
+    self.map.cover[self.x] = 2
--- scenes/Map.gd
+++ scenes/Map.gd
@@ -3,12 +3,26 @@ extends Node2D
 var row = 10
 var col = 10
 var tile = preload("tile.tscn")
+var direction = [-1, +1, -col-1, -col-2, -col-3, col+1, col+2, col+3]
 
 var generated = false
 var map = Array()
+var cover = Array()
+
+func uncover(x):
+  if not generated:
+    return
+  if cover[x] != 0:
+    return
+  cover[x] = 1
+  if map[x] == 0:
+    for t in direction:
+      uncover(x+t)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  print(get_viewport().size)
   for r in row:
     for c in col:
       var t = tile.instantiate()
@@ -22,24 +36,30 @@ func _ready():
 func _process(_delta):
   if not generated:
     map = Array()
+    cover = Array()
     for c in col+2:
-      map.append(0)
+      map.append(-2)
+      cover.append(-2)
     for r in row:
-      map.append(0)
+      map.append(-2)
+      cover.append(-2)
       for c in col:
         var i = 0
         if randf_range(0, 1) < 0.1:
           i = -1
         map.append(i)
-      map.append(0)
+        cover.append(0)
+      map.append(-2)
+      cover.append(-2)
     for c in col+2:
-      map.append(0)
+      map.append(-2)
+      cover.append(-2)
     for r in row:
       for c in col:
         var x = (r+1) * (col+2) + c+1
         if map[x] != 0:
           continue
-        for t in [-1, +1, -col-1, -col-2, -col-3, col+1, col+2, col+3]:
+        for t in direction:
           if map[x+t] == -1:
             map[x] += 1
     generated = true
